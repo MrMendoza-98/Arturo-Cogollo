aa6d63b3064b1cb39b1dae4549292af96dba882d
JacksonGonzalez <jaesgoca@gmail.com>
2018-08-21 21:24:55 -0500

    se arregla los tamaños del menu

--- navigation.php
+++ navigation.php
@@ -3,7 +3,7 @@
                 display: block;
             }
             li a{
-                font-size: 16px;
+                font-size: 18px;
             }
         </style>
     <header class="s-header">
@@ -19,42 +19,23 @@
             <a href="#0" class="header-nav__close" title="close"><span>Close</span></a>
 
             <div class="header-nav__content">
-                <!-- <h3>Menu</h3> -->
 
                 <ul class="header-nav__list">
                     <li class="current"><a class="smoothscroll"  href="index.php" title="home">Home</a></li>
                     <li><a class="smoothscroll"  href="categories.php" title="about">Categories</a></li>
-                    <ul>
-                        <li><a class="" href="category-all.php">All</a></li>
-                        <li><a class="" href="category-houses.php">Houses / New Buildings</a></li>
-                        <li><a class="" href="category-apartments.php">Apartments / Refurb</a></li>
-                        <li><a class="" href="category-cultural.php">Cultural</a></li>
-                        <li><a class="" href="category-commercial.php">Commercial</a></li>
-                        <li><a class="" href="category-plastic.php">Plastic Surgery Clinic</a></li>
-                        <li><a class="" href="category-landscaping.php">Landscaping</a></li>
-                        <li><a class="" href="category-public.php">Public Buildings</a></li>
-                        <li><a class="" href="category-unbuilt.php">Unbuilt</a></li>
+                    <ul class="header-nav-list-category">
+                        <li><a class="smoothscroll" href="category-all.php">All</a></li>
+                        <li><a class="smoothscroll" href="category-houses.php">Houses / New Buildings</a></li>
+                        <li><a class="smoothscroll" href="category-apartments.php">Apartments / Refurb</a></li>
+                        <li><a class="smoothscroll" href="category-cultural.php">Cultural</a></li>
+                        <li><a class="smoothscroll" href="category-commercial.php">Commercial</a></li>
+                        <li><a class="smoothscroll" href="category-plastic.php">Plastic Surgery Clinic</a></li>
+                        <li><a class="smoothscroll" href="category-landscaping.php">Landscaping</a></li>
+                        <li><a class="smoothscroll" href="category-public.php">Public Buildings</a></li>
+                        <li><a class="smoothscroll" href="category-unbuilt.php">Unbuilt</a></li>
                     </ul>
                     <li><a class="smoothscroll"  href="contact.php" title="contact">Contact</a></li>
                 </ul>
-
-                <!-- <ul class="header-nav__social">
-                    <li>
-                        <a href="#"><i class="fa fa-facebook"></i></a>
-                    </li>
-                    <li>
-                        <a href="#"><i class="fa fa-twitter"></i></a>
-                    </li>
-                    <li>
-                        <a href="#"><i class="fa fa-instagram"></i></a>
-                    </li>
-                    <li>
-                        <a href="#"><i class="fa fa-behance"></i></a>
-                    </li>
-                    <li>
-                        <a href="#"><i class="fa fa-dribbble"></i></a>
-                    </li>
-                </ul> -->
 
             </div> <!-- end header-nav__content -->
 
