d05964638348c6f9191a7a2cde1fc43ed7c90afa
MrMendoza-98 <mrmendoza98@outlook.com>
2018-10-14 15:43:58 -0500

    algunos cambios

--- index1.php
+++ index1.php
@@ -32,54 +32,28 @@
     ?>
 
     <section>
-      <div id="carouselExampleIndicators" class="carousel zoom landscape slide" data-ride="carousel">
-        <!-- <ol class="carousel-indicators">
-          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-        </ol> -->
-        <div class="carousel-inner" role="listbox">
-          <!-- Slide One - Set the background image for this slide in the line below -->
-          <a href="detail.php" class="carousel-item active" style="background-image: url('http://www.arturocogollo.com/images/<?php echo '1.jpg'; ?>')">
-          </a>
-          <!-- Slide Two - Set the background image for this slide in the line below -->
-          <a href="detail.php" class="carousel-item" style="background-image: url('http://www.arturocogollo.com/images/<?php echo '2.jpg'; ?>')">
-          </a>
-          <!-- Slide Three - Set the background image for this slide in the line below -->
-          <a href="detail.php" class="carousel-item" style="background-image: url('http://www.arturocogollo.com/images/<?php echo '3.jpg'; ?>')">
-          </a>
-          <!-- Slide Three - Set the background image for this slide in the line below -->
-          <a href="detail.php" class="carousel-item" style="background-image: url('http://www.arturocogollo.com/images/<?php echo '4.jpg'; ?>')">
-          </a>
-          <!-- Slide Three - Set the background image for this slide in the line below -->
-          <a href="detail.php" class="carousel-item" style="background-image: url('http://www.arturocogollo.com/images/<?php echo '5.jpg'; ?>')">
-          </a>
-          
-          <!-- Slide Three - Set the background image for this slide in the line below -->
-          <a href="detail.php" class="carousel-item" style="background-image: url('http://www.arturocogollo.com/images/<?php echo '7.jpg'; ?>')">
-          </a>
-          <!-- Slide Three - Set the background image for this slide in the line below -->
-          <a href="detail.php" class="carousel-item" style="background-image: url('http://www.arturocogollo.com/images/<?php echo '8.jpg'; ?>')">
-          </a>
-          <!-- Slide Three - Set the background image for this slide in the line below -->
-          <a href="detail.php" class="carousel-item" style="background-image: url('http://www.arturocogollo.com/images/<?php echo '9.jpg'; ?>')">
-          </a>
+      <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img class="d-block w-100" src=".../800x400?auto=yes&bg=777&fg=555&text=First slide" alt="First slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src=".../800x400?auto=yes&bg=666&fg=444&text=Second slide" alt="Second slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src=".../800x400?auto=yes&bg=555&fg=333&text=Third slide" alt="Third slide">
+          </div>
         </div>
-        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-          <div class="circle-prev">
-          <n class="fa fa-angle-left"></span>
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
           <span class="sr-only">Previous</span>
-            
-          </div>
         </a>
-        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-          <div class="circle-next">
-          <spaspan class="fa fa-angle-right" ></span>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
           <span class="sr-only">Next</span>
-            
-          </div>
         </a>
       </div>
+      
     </section>
 
     <!-- Bootstrap core JavaScript -->
